9eac5ed35b855907ca21c6e453a1a85946ddc139
katherine-demoya <ktdoyarce@gmail.com>
2025-09-20 09:20:00 +0300

    "Login y conversos de texto a voz y de voz a texto"

---
+++ LoginExpress/app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const port = 3000
+// Get the client
+const mysql = require('mysql2/promise');
+
+// Create the connection to database
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'login',
+});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/login',async (req, res) => {//req = request, peticion ; res = response, respuesta 
+    const datos = req.query;
+    console.log(datos)
+    // A simple SELECT query
+    try {
+        const [results, fields] = await connection.query(
+            "SELECT * FROM `usuarios` WHERE `usuario` = ? AND `clave` = ?",
+            [datos.usuario,datos.clave]
+        );
+        if (results.length>0){
+            res.status(200).send('Inicio de sesión correcto')
+        }else{
+            res.status(401).send('Datos incorrectos')
+        }
+            
+
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+    } catch (err) {
+        console.log(err);
+    }
+    })
+app.get('/validar', (req, res) => {
+    res.send('Sesion Validada!')
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
